3390d369a3cb919bcdc998515ff19dc32f09b0f6
arifes <arifesviryova@gmail.com>
2022-06-10 17:54:58 -0400

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RequestsComponent } from './requests/requests.component';
+import { ReimbursementsComponent } from './reimbursements/reimbursements.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent,
+    ProfileComponent,
+    RequestsComponent,
+    ReimbursementsComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
